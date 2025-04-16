771f0133022eda466c00274f7a2533bd1eb4a5a8
ryo-maekawa <mekw.forbiz@gmail.com>
2025-04-16 16:04:20 +0900

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // ← 静的エクスポートモードにする！
+  trailingSlash: true, // ← ページのURLを「/」で終わらせる
+  images: {
+    unoptimized: true, // ← imageコンポーネント使うなら必要
+  },
+  // basePath: "/リポジトリ名", ← GitHub Pagesのサブディレクトリ運用時に必要！
 };
 
-export default nextConfig;
+module.exports = nextConfig;
