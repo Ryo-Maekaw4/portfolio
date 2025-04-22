a0f03481a280ddd7df3528eef99f3dbc8928dd85
ryo-maekawa <mekw.forbiz@gmail.com>
2025-04-22 21:24:53 +0900

    コンポーネント修正

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -11,20 +11,21 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
         className
       )}
       {...props}
-    />
+    ></div>
   )
 }
+
 
 function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      data-slot="card-header"
+      data-slot="card"
       className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
         className
       )}
       {...props}
-    />
+    ></div>
   )
 }
 
@@ -34,7 +35,7 @@ function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-title"
       className={cn("leading-none font-semibold", className)}
       {...props}
-    />
+    ></div>
   )
 }
 
@@ -44,7 +45,7 @@ function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-description"
       className={cn("text-muted-foreground text-sm", className)}
       {...props}
-    />
+    ></div>
   )
 }
 
@@ -57,7 +58,7 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
         className
       )}
       {...props}
-    />
+    ></div>
   )
 }
 
@@ -67,7 +68,7 @@ function CardContent({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-content"
       className={cn("px-6", className)}
       {...props}
-    />
+    ></div>
   )
 }
 
@@ -77,7 +78,7 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
       data-slot="card-footer"
       className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
       {...props}
-    />
+    ></div>
   )
 }
 
