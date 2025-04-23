03db6e237ef3d941826ad7e367221889d38c2bfb
ryo-maekawa <mekw.forbiz@gmail.com>
2025-04-23 18:55:03 +0900

    経歴カード追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,24 +6,24 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { Badge } from "@/components/ui/badge"
 
-// const Skills = {
-  'php': 'php',
-  'html': 'html',
-  'mysql': 'mysql',
-  'postgres': 'postgres',
-  'microsoft sql server': 'microsoft sql server',
-  'oracle': 'oracle'
-}
+
+// import { ScrollArea } from "@/components/ui/scroll-area"
+
+const Tools: Array<string> = [
+  // 'mac', 'php', 'html', 'mysql', 'postgres', 'microsoft sql server', 'oracle'
+  'Mac', 'PHP', 'zendframework', 'Laravel10', 'MySQL', 'JIRA', 'Confluence', 'Git', 'Slack', 'Gather'
+];
 
 const Careers = {
   1: {
-    'period_from': '2025-08',
-    'period_to': 'now',
-    'project': 'BtoB中間システム保守運用',
-    'detail': '',
-    'member': '',
+    'period_from': '2023-07',
+    'period_to': '2024-07',
+    'project': 'D社toCサービスシステム開発・保守',
+    'detail': '■概要\n顧客の自社サービスのシステム基盤のモダナイズ及び、セキュリティ強化と保守性の担保\n\n■作業詳細\n・モダナイズ対象機能の調査、仕様書作成\n・プログラムコーディング\n・テスト設計\r\n・単体テスト、結合テスト\r\n・モダナイズ外の機能の改修対応',
+    'member': '9',
+    'tools': Tools,
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -35,6 +35,7 @@ const Careers = {
     'project': 'BtoB中間システム保守運用',
     'detail': '',
     'member': '',
+    'tools': '',
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -46,6 +47,7 @@ const Careers = {
     'project': 'BtoB中間システム保守運用',
     'detail': '',
     'member': '',
+    'tools': '',
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -57,6 +59,7 @@ const Careers = {
     'project': 'BtoB中間システム保守運用',
     'detail': '',
     'member': '',
+    'tools': '',
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -68,6 +71,7 @@ const Careers = {
     'project': 'BtoB中間システム保守運用',
     'detail': '',
     'member': '',
+    'tools': '',
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -79,6 +83,7 @@ const Careers = {
     'project': 'BtoB中間システム保守運用',
     'detail': '',
     'member': '',
+    'tools': '',
     'position': 'SE',
     'fase': {
       1: ['基本設計', '詳細設計', '開発', 'テスト', '保守運用']
@@ -86,15 +91,23 @@ const Careers = {
   },
 }
 
+const calcMonths = (period_from: string, period_to: string) => {
+  const from = new Date(period_from);
+  const to = new Date(period_to);
+  const diff = to.getTime() - from.getTime();
+  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  return months;
+}
+
 export default function Home() {
   return (
-    <main className="min-h-screen items-center justify-center p-8">
-      <div className="flex">
+    <main className="min-h-screen items-center justify-center p-8 w-full ">
+      <div className="">
         <Card className="max-w-md w-full shadow-xl rounded-2xl">
           <CardContent className="p-6">
             <div className="flex">
               <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarImage src="/assets/img/avatar.png" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
 
@@ -103,21 +116,55 @@ export default function Home() {
             <p className="text-gray-600">システムエンジニア / PHP, Laravel, React 勉強中</p>
           </CardContent>
         </Card>
+      </div>
 
-        <Card className="max-w-md w-full shadow-xl rounded-2xl">
+      <div className="w-full">
+        <Card className="w-full shadow-xl rounded-2xl">
           <CardContent className="p-6">
+            <h1>Careers</h1>
             {/* <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4"> */}
             <Accordion type="single" collapsible>
-              <AccordionItem value="item-1">
-                <AccordionTrigger>Careers</AccordionTrigger>
                 {Object.keys(Careers).map((key) => {
+                  const data = Careers[key]
                   return (
-                    <AccordionContent key={key}>
-                      {Careers[key].project} {Careers[key].period_from} ~ {Careers[key].period_to}
-                    </AccordionContent>
-                  )
+                    <AccordionItem value={Careers[key]}>
+                      <AccordionTrigger>
+                        <p className="font-bold">{data.project} {data.period_from} ~ {data.period_to}（{calcMonths(data.period_from, data.period_to)}ヶ月）</p>
+                      </AccordionTrigger>
+                      <AccordionContent key={key}>
+                        <p>メンバー数: {data.member}</p>
+                        <p>ポジション: {data.position}</p>
+                        <div className="grid grid-cols-3">
+                          <Card className="col-span-2 rounded-2xl">
+                            <CardContent className="p-6">
+                              <p className="text-gray-600 whitespace-pre-wrap">{data.detail}</p>
+                            </CardContent>
+                          </Card>
+
+                          <Card className="rounded-2xl">
+                            <CardContent className="p-6">
+                              <ul className="list-disc list-inside">
+                                {Object.values(data.fase).flat().map((fase, index) => (
+                                  <li key={index}>{fase}</li>
+                                ))}
+                              </ul>
+                            </CardContent>
+                          </Card>
+                        </div>
+
+                        <Card className="rounded-2xl">
+                          <CardContent className="p-6">
+                            {Object.values(data.tools).flat().map((tool) => (
+                              <Badge variant="outline">{tool}</Badge>
+                            ))}
+                          </CardContent>
+                        </Card>
+
+
+                      </AccordionContent>
+                    </AccordionItem>
+                  );
                 })}
-              </AccordionItem>
             </Accordion>
             {/* </ScrollArea> */}
 
